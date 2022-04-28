59619a29e936a45e4fb987ea89424a873b6b7188
Ryan <ryan@geekful.dev>
2022-04-29 00:26:12 +0800

    Create main layout of landing page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Head from 'next/head'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Better Schema Viewer</title>
-      </Head>
-      <div>Hey</div>
-    </>
+    <Layout>
+      <div className='container mt-5'>
+        <div className='row'>
+          <h1 className='text-center mt-5 fw-light'>A Better Schema Viewer</h1>
+        </div>
+        <div className='row mt-5'>
+          <div className='col'></div>
+          <div className='col-8'>
+          <input type="text" className="form-control" id="searchBox" placeholder="Search for a schema"/>
+          </div>
+          <div className='col'></div>
+        </div>
+      </div>
+    </Layout>
   )
 }
