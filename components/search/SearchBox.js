3b88ac68028461c594759df051263f042acfb54f
Ryan <ryan@geekful.dev>
2022-04-29 10:35:45 +0800

    Lift state up from SearchResults to SearchBox

--- components/search/SearchBox.js
+++ components/search/SearchBox.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import SearchResults from './SearchResults'
+import schemaList from '../../schemas/schema-list'
 
 export default class SearchBox extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedSchema: 'Cm-Something'
+    }
+  }
+
   render() {
+    const filteredSchemaList = schemaList;
+
     return (
       <>
-        <input type="text" className="form-control" id="searchBox" placeholder="Search for a schema"/>
-        <SearchResults/>
+        <input type="text" className="form-control" id="searchBox" placeholder="Search for a schema" />
+        <SearchResults schemaList={filteredSchemaList} selectedSchema={this.state.selectedSchema}/>
       </>
     )
   }
--- components/search/SearchResults.js
+++ components/search/SearchResults.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react'
-import schemaList from '../../schemas/schema-list'
 import styles from '../../styles/SearchResults.module.css'
 
 export class SearchResults extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selected: 'Cm-Something'
-    }
-  }
-
   render() {
     return (
       <div className={styles.resultsBox}>
-        {schemaList.map(schema => 
-          <p key={schema.name} className={schema.name === this.state.selected ? styles.selected : ''}>
+        {this.props.schemaList.map(schema => 
+          <p key={schema.name} className={schema.name === this.props.selectedSchema ? styles.selected : ''}>
             {schema.name}
             <span className={styles.schemaType}>{schema.type}</span>
           </p>
