7b05377c71d2fd32f92ca0f76298c7a49524112e
Ryan <ryan@geekful.dev>
2022-04-29 11:34:27 +0800

    Remove logs

--- components/search/SearchBox.js
+++ components/search/SearchBox.js
@@ -48,8 +48,6 @@ export default class SearchBox extends Component {
       return schema.name === mouseSelectedSchema
     })
 
-    console.log(selectedSchemaIndex)
-
     this.setState({
       selectedSchemaIndex
     })
