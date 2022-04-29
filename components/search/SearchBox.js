55e35ded531a158ec3357ec0e63ca407a95f4218
Ryan <ryan@geekful.dev>
2022-04-29 11:18:05 +0800

    Make arrow keys change the selected result

--- components/search/SearchBox.js
+++ components/search/SearchBox.js
@@ -2,36 +2,51 @@ import React, { Component } from 'react'
 import SearchResults from './SearchResults'
 import schemaList from '../../schemas/schema-list'
 
+const KEY_ARROW_UP = 38;
+const KEY_ARROW_DOWN = 40;
+
 export default class SearchBox extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      searchQuery: '',
-      selectedSchema: 'Cm-Something',
+      filteredSchemaList: [],
+      selectedSchemaIndex: 0
     }
 
     this.handleSearchQueryChange = this.handleSearchQueryChange.bind(this);
+    this.handleChangeSelectedSchema = this.handleChangeSelectedSchema.bind(this);
   }
 
   handleSearchQueryChange(event) {
     this.setState({
-      searchQuery: event.target.value
+      filteredSchemaList: schemaList.filter(schema => {
+        return schema.name.toLowerCase().includes(event.target.value.toLowerCase())
+      }),
+      selectedSchemaIndex: 0
     })
   }
 
-  render() {
-    const filteredSchemaList = this.state.searchQuery.trim() === '' ? [] : schemaList.filter(schema => {
-      return schema.name.toLowerCase().includes(this.state.searchQuery.toLowerCase())
-    })
+  handleChangeSelectedSchema(event) {
+    if (event.keyCode === KEY_ARROW_UP) {
+      this.setState({
+        selectedSchemaIndex: this.state.selectedSchemaIndex === 0 ? 0 : this.state.selectedSchemaIndex - 1
+      })
+    } else if (event.keyCode === KEY_ARROW_DOWN) {
+      this.setState({
+        selectedSchemaIndex: this.state.filteredSchemaList.length === this.state.selectedSchemaIndex + 1 ? this.state.selectedSchemaIndex : this.state.selectedSchemaIndex + 1
+      })
+    }
+  }
 
-    const selectedSchema = filteredSchemaList.length > 0 ? filteredSchemaList[0].name : ''
+  render() {
+    const selectedSchema = this.state.filteredSchemaList.length > 0 ? this.state.filteredSchemaList[this.state.selectedSchemaIndex].name : []
 
     return (
       <>
-        <input type="text" className="form-control" id="searchBox" placeholder="Search for a schema" onChange={this.handleSearchQueryChange} />
-        {filteredSchemaList.length > 0 ? <SearchResults schemaList={filteredSchemaList} selectedSchema={selectedSchema} /> : null}
+        <input type="text" className="form-control" id="searchBox" placeholder="Search for a schema" onChange={this.handleSearchQueryChange} onKeyDown={this.handleChangeSelectedSchema} />
+        {this.state.filteredSchemaList.length > 0 ? <SearchResults schemaList={this.state.filteredSchemaList} selectedSchema={selectedSchema} /> : null}
       </>
     )
   }
