265e1747f04674b77fda1a241f4452f0a03ebb0c
Ryan <ryan@geekful.dev>
2022-04-29 10:41:53 +0800

    Hide the search results if there is search query

--- components/search/SearchBox.js
+++ components/search/SearchBox.js
@@ -8,8 +8,17 @@ export default class SearchBox extends Component {
     super(props);
 
     this.state = {
-      selectedSchema: 'Cm-Something'
+      searchQuery: '',
+      selectedSchema: 'Cm-Something',
     }
+
+    this.handleSearchQueryChange = this.handleSearchQueryChange.bind(this);
+  }
+
+  handleSearchQueryChange(event) {
+    this.setState({
+      searchQuery: event.target.value
+    })
   }
 
   render() {
@@ -17,8 +26,8 @@ export default class SearchBox extends Component {
 
     return (
       <>
-        <input type="text" className="form-control" id="searchBox" placeholder="Search for a schema" />
-        <SearchResults schemaList={filteredSchemaList} selectedSchema={this.state.selectedSchema}/>
+        <input type="text" className="form-control" id="searchBox" placeholder="Search for a schema" onChange={this.handleSearchQueryChange} />
+        {this.state.searchQuery.trim() != '' ? <SearchResults schemaList={filteredSchemaList} selectedSchema={this.state.selectedSchema}/> : null}
       </>
     )
   }
