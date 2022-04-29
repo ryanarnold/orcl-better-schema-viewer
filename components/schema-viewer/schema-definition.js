885d428df577b8c373213527d2acca0ac5cb2068
Ryan <ryan@geekful.dev>
2022-04-29 22:30:04 +0800

    Add schema-definition component

---
+++ components/schema-viewer/schema-definition.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SchemaDefinition({ xml }) {
+  return <pre>{xml}</pre>;
+}
+
+export default SchemaDefinition;
--- components/schema-viewer/schema-viewer.js
+++ components/schema-viewer/schema-viewer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import XpathViewer from './xpath-viewer';
+import SchemaDefinition from './schema-definition';
 
 export class SchemaViewer extends Component {
   constructor(props) {
@@ -14,9 +15,7 @@ export class SchemaViewer extends Component {
         <h1 className="fw-lighter mt-3">{schema.name}</h1>
         <h6 className="text-secondary fw-lighter">{schema.type}</h6>
         <div className="border rounded p-2 mt-3 overflow-scroll code-div">
-          <pre>
-            {schema.xml}
-          </pre>
+            <SchemaDefinition xml={schema.xml}/>
         </div>
         <XpathViewer />
       </div>
