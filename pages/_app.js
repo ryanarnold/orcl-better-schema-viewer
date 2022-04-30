7e4d7513b3b82ddcddf96c4c65a733a60ce8ad8b
Ryan <ryan@geekful.dev>
2022-04-30 13:41:50 +0800

    Fix eslint build issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
 
